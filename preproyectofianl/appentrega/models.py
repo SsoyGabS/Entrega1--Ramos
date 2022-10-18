ff88ce6d16b60be7deadeff33a345551a7443d94
Gabriel Ramos <ramos13gabriel93@gmail.com>
2022-10-17 23:05:21 -0300

    tercer carga

--- preproyectofianl/appentrega/models.py
+++ preproyectofianl/appentrega/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Familia(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    edad = models.IntegerField()
